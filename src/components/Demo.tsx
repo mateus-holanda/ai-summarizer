987b85b7ea79a62b1944e7e6fc13c58c4dfe6e19
Mateus Holanda <mateus23ita@gmail.com>
2023-07-17 13:34:09 -0300

    Remove type from allArticles

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -17,7 +17,7 @@ export function Demo() {
     url: '',
     summary: '',
   })
-  const [allArticles, setAllArticles] = useState<Article[]>([article])
+  const [allArticles, setAllArticles] = useState([article])
   /* const [copied, setCopied] = useState('') */
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
